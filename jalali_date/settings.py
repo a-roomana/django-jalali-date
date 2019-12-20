3b3d03c08e09dcaa00ae25142e9302867a5b4193
Arman Roomana <roomana.arman@gmail.com>
2019-12-20 16:18:26 +0330

    Update README.md and remove warning settings

--- jalali_date/settings.py
+++ jalali_date/settings.py
@@ -31,16 +31,7 @@ if hasattr(settings, 'JALALI_DATE_DEFAULTS'):
     jalali_date_defaults = settings.JALALI_DATE_DEFAULTS
     for item in jalali_date_defaults.keys():
         if isinstance(jalali_date_defaults[item], dict):
-            if item == 'STRFTIME':
-                JALALI_DATE_DEFAULTS['Strftime'].update(jalali_date_defaults[item])
-                print(
-                    '>>>\t \x1b[%sm%s\x1b[0m' % (
-                        '31',
-                        'Django-Jalali-Date: "STRFTIME" is removed in version 0.3, please replace it by "Strftime"'
-                    )
-                )
-            else:
-                JALALI_DATE_DEFAULTS[item].update(jalali_date_defaults[item])
+            JALALI_DATE_DEFAULTS[item].update(jalali_date_defaults[item])
         else:
             JALALI_DATE_DEFAULTS[item] = jalali_date_defaults[item]
 
