3967114a322af1d1cdc4357740ca75242969901b
Mojix Coder <mojixcoder@gmail.com>
2021-12-07 20:07:38 +0330

    Change import orders

--- jalali_date/widgets.py
+++ jalali_date/widgets.py
@@ -8,9 +8,9 @@ from django.utils.html import format_html
 from jdatetime import GregorianToJalali
 
 try:
-    from django.utils.translation import ugettext as _  # Django < 4
-except ImportError:
     from django.utils.translation import gettext as _  # Django >= 4
+except ImportError:
+    from django.utils.translation import ugettext as _  # Django < 4
 
 
 class AdminJalaliDateWidget(AdminDateWidget):
