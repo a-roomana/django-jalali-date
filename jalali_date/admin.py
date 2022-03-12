85f4005a1c5decdeae56fd7581e8e372f4920467
AmirMahmood <amma4ww@gmail.com>
2022-03-13 01:48:35 +0330

    Add support for Auto converting list_display fields to jalali

--- jalali_date/admin.py
+++ jalali_date/admin.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.core.exceptions import FieldDoesNotExist
 from django.db import models
 
+from jalali_date import date2jalali, datetime2jalali
 from jalali_date.fields import JalaliDateField, SplitJalaliDateTimeField
 from jalali_date.widgets import AdminJalaliDateWidget, AdminSplitJalaliDateTime
 
@@ -24,6 +27,39 @@ class ModelAdminJalaliMixin(object):
         formfield_overrides.update(self.formfield_overrides)
         self.formfield_overrides = formfield_overrides
         super(ModelAdminJalaliMixin, self).__init__(*args, **kwargs)
+
+    def get_list_display(self, request):
+        list_display = list(super(ModelAdminJalaliMixin, self).get_list_display(request))
+        for index, field_name in enumerate(list_display[:]):
+            try:
+                field = self.opts.get_field(field_name)
+            except FieldDoesNotExist:
+                pass
+            else:
+                func_name = 'get_jalali_' + list_display[index]
+                if isinstance(field, models.DateTimeField):
+                    list_display[index] = func_name
+                    setattr(self, func_name, self.jalali_datetime_list_display(field))
+                elif isinstance(field, models.DateField):
+                    list_display[index] = func_name
+                    setattr(self, func_name, self.jalali_date_list_display(field))
+        return list_display
+
+    def jalali_datetime_list_display(self, field):
+        def func(obj):
+            strftime = settings.JALALI_DATE_DEFAULTS['Strftime']['datetime']
+            return datetime2jalali(getattr(obj, field.name)).strftime(strftime)
+
+        func.short_description = field.verbose_name
+        return func
+
+    def jalali_date_list_display(self, field):
+        def func(obj):
+            strftime = settings.JALALI_DATE_DEFAULTS['Strftime']['date']
+            return date2jalali(getattr(obj, field.name)).strftime(strftime)
+
+        func.short_description = field.verbose_name
+        return func
 
 
 class StackedInlineJalaliMixin(object):
