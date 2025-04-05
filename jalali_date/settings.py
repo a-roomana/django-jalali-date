25fdd3a9cd04bbbe10085072131f3f82530a73a7
partizaans <r.pourmeshki@gmail.com>
2025-04-05 13:50:12 +0330

    Replace outdated datepicker CSS and JS with Jalali Datepicker.
    Switched to a newer Jalali datepicker version with minimal styles and improved modern compatibility. Removed the legacy jQuery UI dependencies as they were outdated and no longer necessary.

--- jalali_date/settings.py
+++ jalali_date/settings.py
@@ -11,16 +11,11 @@ JALALI_DATE_DEFAULTS = {
     'Static': {
         'js': [
             'admin/js/django_jalali.min.js',
-            # or
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
+            'admin/js/prevent_default_listener.min.js'
         ],
         'css': {
             'all': [
-                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+                'admin/css/django_jalali.min.css',
             ]
         }
     },
